65e9ee74cb22f9c730ef2d7fbb40b713ef4db42d
DavidSheltonSF <david.faria012@gmail.com>
2026-08-09 16:50:54 -0300

    refactor(WhatsAppButton): update to accept icon

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -1,23 +1,25 @@
-import { buildWhatsAppUrl } from "@/services/whatsapp";
-import { Button, ButtonProps } from "./ui/Button";
-import { whatsappConfig } from "@/lib/config";
-import { PropsWithChildren } from "react";
-
+import { buildWhatsAppUrl } from '@/services/whatsapp';
+import { Button, ButtonProps } from './ui/Button';
+import { whatsappConfig } from '@/lib/config';
+import { PropsWithChildren, ReactNode } from 'react';
 
 interface Props {
-  className?: string
+  className?: string;
+  icon: ReactNode;
 }
 
-export function WhatsAppButton({children, className}: PropsWithChildren<Props>) {
-  return ( 
-  <Button 
-    className={className}
-    as="a" 
-    href={buildWhatsAppUrl(whatsappConfig.defaultMessage)} 
-    target="_blank" 
-    rel="noopener noreferrer" 
-    size="md">
-    {children}
-  </Button>
-  )
+export function WhatsAppButton({ children, className, icon }: PropsWithChildren<Props>) {
+  return (
+    <Button
+      className={className}
+      as="a"
+      href={buildWhatsAppUrl(whatsappConfig.defaultMessage)}
+      target="_blank"
+      rel="noopener noreferrer"
+      size="md"
+      icon={icon}
+    >
+      {children}
+    </Button>
+  );
 }
